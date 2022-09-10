cbe9e9138a9f609e1c83c67774fb41a280a18a80
VladimirPariy <Vladimir.pariy@gmail.com>
2022-09-11 00:49:27 +0300

    change index css

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/Reset.scss'
-import './styles/index.scss';
+import './styles/index.css';
 
 import App from './App';
 
